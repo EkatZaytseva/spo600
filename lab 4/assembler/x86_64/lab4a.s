aa18310014bb7fa8233f7a083f41138a223b7bec
Ekateria Zaytseva <e_zaytseva@outlook.com>
2018-10-22 13:28:54 -0400

    Fix lab4a comments

--- lab 4/assembler/x86_64/lab4a.s
+++ lab 4/assembler/x86_64/lab4a.s
@@ -3,19 +3,19 @@
 
 start = 0                       /* starting value for the loop index; note that this is a symbol (constant), not a variable */
 max = 10                        /* loop exits when the index hits this number (loop condition is i<max) */
-cvt = 48
+cvt = 48			/* this is a number that needs to be added to the inary value of the registry to change value to decimal */
 
 _start:
     mov     $start,%r15         /* loop index */
 
 loop:
-    /* ... body of the loop ... do something useful here ... */
+    /* body of the loop */
     movq    $len,%rdx
     movq    $msg,%rsi
     movq    $cvt,%r14
     movq    %r15,%r13
     add     %r13,%r14 
-    movb    %r14b,msg+6
+    movb    %r14b,msg+6		/* treating the string to be printed as a pointer, so msg+6 points to the 6th character */
     movq    $1,%rdi
     movq    $1,%rax
    
